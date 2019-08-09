b1065f37e32940feb52e7eae4bb6893274682255
Hernan Guardado <hernancitogg@gmail.com>
2019-08-09 18:00:37 -0400

    refactoring for maven project

---
+++ BankApp/src/main/java/application/Domain/Account.java
@@ -0,0 +1,10 @@
+package application.Domain;
+
+import java.util.ArrayList;
+
+public class Account {
+    private int accountNumber;
+    private double balance;
+    private ArrayList<User> users;
+
+}
--- BankApp/src/main/java/test/RequirementsTests.java
+++ BankApp/src/main/java/test/RequirementsTests.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import application.FileEdit;
+import application.Dao.FileEdit;
 
 public class RequirementsTests {
 	
